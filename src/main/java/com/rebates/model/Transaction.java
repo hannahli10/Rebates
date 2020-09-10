9d660d28dfdb0d08d7ab630d30c73b3042fd0bd0
hannah <hannah@hannahdeMacBook-Pro.local>
2020-09-09 20:03:03 -0400

    add setter and getter in Transaction

--- src/main/java/com/rebates/model/Transaction.java
+++ src/main/java/com/rebates/model/Transaction.java
@@ -6,5 +6,38 @@ public class Transaction {
     private long id;
     private long purchaseTime;
     private String orderId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPurchaseTime() {
+        return purchaseTime;
+    }
+
+    public void setPurchaseTime(long purchaseTime) {
+        this.purchaseTime = purchaseTime;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
     private BigDecimal amount;
 }
