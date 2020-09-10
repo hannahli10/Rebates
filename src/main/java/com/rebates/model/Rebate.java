2f03fc5aa7cde448d83e8abbb62868cd58c54343
hannah <hannah@hannahdeMacBook-Pro.local>
2020-09-09 20:02:40 -0400

    add setter and getter in Rebate

--- src/main/java/com/rebates/model/Rebate.java
+++ src/main/java/com/rebates/model/Rebate.java
@@ -6,6 +6,63 @@ public class Rebate {
     private long id;
     private String name;
     private String link;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getRebateType() {
+        return rebateType;
+    }
+
+    public void setRebateType(String rebateType) {
+        this.rebateType = rebateType;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
     private String rebateType;
     private BigDecimal value;
     private long startTime;
