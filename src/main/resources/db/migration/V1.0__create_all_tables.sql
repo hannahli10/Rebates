f0550017988b7b0949905b4abd05448d66477fbb
hannah <hannah@hannahdeMacBook-Pro.local>
2020-09-16 23:29:25 -0400

    modified sql

--- src/main/resources/db/migration/V1.0__create_all_tables.sql
+++ src/main/resources/db/migration/V1.0__create_all_tables.sql
@@ -1,14 +1,14 @@
-CREATE SEQUENCE provider_id_seq START WITH 1;
-CREATE SEQUENCE rebate_id_seq START WITH 1;
+-- CREATE SEQUENCE provider_id_seq START WITH 1;
+-- CREATE SEQUENCE rebate_id_seq START WITH 1;
 
-CREATE TABLE provider(
+CREATE TABLE providers(
 /*id             INTEGER NOT NULL default nextval(‘account_id_seq’),*/
     id                BIGSERIAL NOT NULL,
     name              VARCHAR(30) not null unique
 );
-ALTER TABLE provider ADD CONSTRAINT provider_pk PRIMARY KEY ( id );
 
-CREATE TABLE rebate(
+
+CREATE TABLE rebates(
 /*id             INTEGER NOT NULL default nextval(‘account_id_seq’),*/
     id                BIGSERIAL NOT NULL,
     name              VARCHAR(30) not null unique,
@@ -19,19 +19,22 @@ CREATE TABLE rebate(
     end_time          TIMESTAMP,
     provider_id       BIGINT NOT NULL
 );
-ALTER TABLE rebate ADD CONSTRAINT rebate_pk PRIMARY KEY ( id );
 
-CREATE TABLE transaction(
+
+CREATE TABLE transactions(
 /*id             INTEGER NOT NULL default nextval(‘account_id_seq’),*/
     id                         BIGSERIAL NOT NULL,
     purchase_time              TIMESTAMP default CURRENT_TIMESTAMP,
     order_id                   VARCHAR(30),
     amount                     NUMERIC(10, 2),
-    rebate_id                  BIGINT NOT NULL
+    rebate_id                 BIGINT NOT NULL
 );
-ALTER TABLE transaction ADD CONSTRAINT transaction_pk PRIMARY KEY ( id );
 
 
-ALTER TABLE rebate ADD CONSTRAINT rebate_provider_fk FOREIGN KEY (provider_id) REFERENCES provider (id);
+ALTER TABLE providers ADD CONSTRAINT provider_pk PRIMARY KEY ( id );
+ALTER TABLE rebates ADD CONSTRAINT rebate_pk PRIMARY KEY ( id );
+ALTER TABLE transactions ADD CONSTRAINT transaction_pk PRIMARY KEY ( id );
 
-ALTER TABLE transaction ADD CONSTRAINT transaction_rebate_fk FOREIGN KEY (rebate_id) REFERENCES rebate (id);
+
+ALTER TABLE rebates ADD CONSTRAINT rebate_provider_fk FOREIGN KEY (provider_id) REFERENCES providers (id);
+ALTER TABLE transactions ADD CONSTRAINT transaction_rebate_fk FOREIGN KEY (rebate_id) REFERENCES rebates (id);
--- src/main/resources/db/migration/V1.1__insert_data_into_p_r_t_tables.sql
+++ src/main/resources/db/migration/V1.1__insert_data_into_p_r_t_tables.sql
@@ -1,16 +1,16 @@
-Insert into Provider (name) values
+Insert into Providers (name) values
 ('Amazon'),
 ('Sephora'),
 ('Walmart');
 commit;
 
-Insert into Rebate (name,link,rebate_type,value,end_time,provider_id) values
+Insert into Rebates (name,link,rebate_type,value,end_time,provider_id) values
 ('Topcashback','www.topcashback.com','Cashback','0.01','08-10-2021 13:10:02.047','1'),
 ('Rakuten','www.rakuten.com','Cashback','0.08','08-15-2021 13:10:02.047','2'),
 ('United Mileage','https://shopping.mileageplus.com/','Credit card points','3','09-10-2020 13:10:02.047','3');
 commit;
 
-Insert into Transaction (purchase_time,order_id,amount,rebate_id) values
+Insert into Transactions (purchase_time,order_id,amount,rebate_id) values
 ('6/20/2020 4:00:00','SF000001','100','1'),
 ('7/14/2020 15:00:00','SG00004','256','3'),
 ('8/24/2020 10:04:00','46575474','23','2');
